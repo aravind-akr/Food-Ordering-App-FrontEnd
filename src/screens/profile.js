4a35cd33fba21fdecde89d05d4874d639ddc0555
Aravind KR <kraravind1993@gmail.com>
2019-07-07 22:18:30 +0530

    Added details and profile pages

---
+++ src/screens/profile.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+
+class ProfilePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div>Profile Page</div>
+            </Fragment>
+        )
+    }
+}
+
+export default ProfilePage;
